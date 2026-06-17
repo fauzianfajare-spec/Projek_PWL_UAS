a241afbdaaf3836317979b1b15a70d09ada1bb9c
Fauzian Fajar <fauzianfajar98@gmail.com>
2026-06-17 12:25:27 +0800

    Update project

--- owichat/settings.py
+++ owichat/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'owichat.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.dummy',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
