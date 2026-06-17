7fdc63149e7864ae9e61a21a8f73cd1c3e99d8e3
Fauzian Fajar <fauzianfajar98@gmail.com>
2026-06-17 12:39:20 +0800

    Fix Render CSRF settings

--- owichat/settings.py
+++ owichat/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'channels',
     'chat',
     'users',
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'owichat.urls'
